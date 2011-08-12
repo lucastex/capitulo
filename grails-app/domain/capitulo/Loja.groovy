6d1d41508434e2d9eff603fb4a7dec72144279aa
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-12 14:26:10 -0300

    Campos da domain class Loja e constraints de validacao

--- grails-app/domain/capitulo/Loja.groovy
+++ grails-app/domain/capitulo/Loja.groovy
@@ -1,7 +1,16 @@
 package capitulo
 
 class Loja {
+	
+	String nome
+	String endereco
+	Integer numero
+	String potencia
 
     static constraints = {
+		nome(nullable: false, blank: false)
+		endereco(nullable: true, blank: true)
+		numero(nullable: false, blank: false, min: 0)
+		potencia(nullable: false, blank: false, inList: ["GOB", "COMAB", "GL"])
     }
 }
