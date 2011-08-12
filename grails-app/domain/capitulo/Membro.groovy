6af92d19189c015c2c0fc8aade1616de32f854e1
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-12 16:11:10 -0300

    Criacao das tabelas e constraint dos Membros

--- grails-app/domain/capitulo/Membro.groovy
+++ grails-app/domain/capitulo/Membro.groovy
@@ -2,6 +2,21 @@ package capitulo
 
 class Membro {
 
+	String nome
+	String email
+	Integer cid
+	Date iniciacao
+	Date elevacao
+	Date investidura
+	Date chevalier
+	
     static constraints = {
+		nome(nullable: false, blank: false)
+		email(nullable: false, blank: false)
+		cid(nullable: false, blank: false, min: 0)
+		iniciacao(nullable: true, blank: true)
+		elevacao(nullable: true, blank: true)
+		investidura(nullable: true, blank: true)
+		chevalier(nullable: true, blank: true)
     }
 }
