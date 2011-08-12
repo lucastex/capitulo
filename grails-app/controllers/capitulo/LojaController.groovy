ce01e1a9f3bd7c5d53499e1fbcfd7d5ef295b719
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-12 15:29:57 -0300

    Criacao de objeto Loja quando a tabela for NULL

--- grails-app/controllers/capitulo/LojaController.groovy
+++ grails-app/controllers/capitulo/LojaController.groovy
@@ -7,10 +7,11 @@ class LojaController {
 
     def index = { 
 	
-		def l1 = new Loja(nome:"Loja Maconica", numero:123, endereco:"Av Avenida", potencia:"GOB")
-		
-		l1.save()
-	
+		def loja = Loja.findAll()
+		if(loja.size() == 0){
+			def l1 = new Loja(nome:"Loja Maconica", numero:123, endereco:"Av Avenida", potencia:"GOB")
+			l1.save()
+		}
 		def allLojas = Loja.findAll()
 		[lojas: allLojas]
 	
