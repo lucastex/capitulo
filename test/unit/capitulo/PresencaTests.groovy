cd93353303002f4e56cfc01ee7bd8e67f73aa922
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 15:16:05 -0300

    Criação do domain class da Presenca

---
+++ test/unit/capitulo/PresencaTests.groovy
@@ -0,0 +1,17 @@
+package capitulo
+
+import grails.test.*
+
+class PresencaTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
