cfd012573993a1d802f673455498c70ef57bbb4e
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 11:57:59 -0300

    Adicao do metodo Editar no controller Gestao

--- grails-app/controllers/capitulo/GestaoController.groovy
+++ grails-app/controllers/capitulo/GestaoController.groovy
@@ -8,6 +8,28 @@ class GestaoController {
 		[gestoes: allGestoes, membros: membros]
 	}
 	
+	def load = {
+		def gestao = Gestao.get(params.id)
+		def membros = Membro.findAll()
+		if(gestao){
+			[gestao: gestao, membros: membros]
+		} else {
+			flash.message = "Não foi possivel editar a gestão"
+			redirect action: "index"
+		}
+	}
+	
+	def update = {
+		def gestao = Gestao.get(params.id)
+		gestao.properties = params
+		if(gestao.save()){
+			flash.message = "Gestão atualizada com sucesso"
+		} else {
+			flash.message = "Não foi possível atualizar a Gestão"
+		}
+		redirect action: "index"
+	}
+	
 	def save = {
 		def gestao = new Gestao(params)
 		if(gestao.save()){
