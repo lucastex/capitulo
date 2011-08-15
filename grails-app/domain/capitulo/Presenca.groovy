181a16f2580b33dab83113e289a900d9ce0fbc96
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 15:18:36 -0300

    Criação das tabelas e constraints da Presenca

--- grails-app/domain/capitulo/Presenca.groovy
+++ grails-app/domain/capitulo/Presenca.groovy
@@ -1,7 +1,14 @@
 package capitulo
 
 class Presenca {
+	
+	Membro membro
+	Reuniao reuniao
+	String status
 
     static constraints = {
+		membro(nullable: false, blank: false)
+		reuniao(nullable: false, blank: false)
+		status(nullable: false, blank: false)
     }
 }
