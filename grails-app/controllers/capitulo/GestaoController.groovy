0c27b0d02460a4d500eec51f224193cc8076b387
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 10:28:10 -0300

    Criação do controller Gestão

---
+++ grails-app/controllers/capitulo/GestaoController.groovy
@@ -0,0 +1,6 @@
+package capitulo
+
+class GestaoController {
+
+    def index = { }
+}
---
+++ test/unit/capitulo/GestaoControllerTests.groovy
@@ -0,0 +1,17 @@
+package capitulo
+
+import grails.test.*
+
+class GestaoControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
