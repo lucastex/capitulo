19d08f2b48a342edffa5fb36256ae484677a8e25
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 14:18:16 -0300

    Criação das tabelas e constraints da domain class Reunião

--- grails-app/domain/capitulo/Reuniao.groovy
+++ grails-app/domain/capitulo/Reuniao.groovy
@@ -1,7 +1,14 @@
 package capitulo
 
 class Reuniao {
+	
+	Gestao gestao
+	Date data
+	Loja loja
 
     static constraints = {
+		gestao(nullable: false, blank:false)
+		data(nullable: false, blank:false)
+		loja(nullable: false, blank:false)
     }
 }
