46dab751d6e594b7914fab2e0c3f08bd71c3e76e
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 10:34:36 -0300

    Criação da action SAVE no controller Gestao

--- grails-app/controllers/capitulo/GestaoController.groovy
+++ grails-app/controllers/capitulo/GestaoController.groovy
@@ -7,4 +7,14 @@ class GestaoController {
 		[gestoes: allGestoes]
 	}
 	
+	def save = {
+		def gestao = new Gestao(params)
+		if(gestao.save()){
+			flash.message = "Gestão criado com sucesso"
+		}else{
+			flash.message = "Ocorreu erro ao salvar a Gestão"
+		}
+		redirect action: "index"
+	}
+	
 }
