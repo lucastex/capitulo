897f8160ec905359c7cfb49ba8043bb5e3c2bd06
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 10:27:02 -0300

    Construção das tabelas e constraints na Classe de Dominio Gestao

--- grails-app/domain/capitulo/Gestao.groovy
+++ grails-app/domain/capitulo/Gestao.groovy
@@ -1,7 +1,18 @@
 package capitulo
 
 class Gestao {
+	
+	Integer ano
+	Integer semestre
+	Integer mc
+	Integer 1c
+	Integer 2c
 
     static constraints = {
+		ano(nullable:false blank:false)
+		semstre(nullable:false blank:false)
+		mc(nullable:false blank:false)
+		1c(nullable:false blank:false)
+		2c(nullable:false blank:false)
     }
 }
