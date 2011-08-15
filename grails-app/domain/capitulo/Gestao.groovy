824fd8e4dfc0705ddea9d1a83a22892b7ab8c161
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-15 10:47:42 -0300

    Edição das tabelas MC, 1C, 2C para MMC, M1C e M2C no domain class Gestao

--- grails-app/domain/capitulo/Gestao.groovy
+++ grails-app/domain/capitulo/Gestao.groovy
@@ -4,15 +4,15 @@ class Gestao {
 	
 	Integer ano
 	Integer semestre
-	Integer mc
-	Integer 1c
-	Integer 2c
+	Integer mmc
+	Integer m1c
+	Integer m2c
 
     static constraints = {
 		ano(nullable:false blank:false)
 		semstre(nullable:false blank:false)
-		mc(nullable:false blank:false)
-		1c(nullable:false blank:false)
-		2c(nullable:false blank:false)
+		mmc(nullable:false blank:false)
+		m1c(nullable:false blank:false)
+		m2c(nullable:false blank:false)
     }
 }
