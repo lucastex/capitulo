62430ee2d7945dd6cab7c6a54b3bbda93ef14e8e
Victor Vasconcelos <flextronvasconcelos@gmail.com>
2011-08-16 15:53:07 -0300

    Criacao da TagLib PresencaUsers

---
+++ grails-app/taglib/capitulo/PresencaUsersTagLib.groovy
@@ -0,0 +1,5 @@
+package capitulo
+
+class PresencaUsersTagLib {
+
+}
---
+++ test/unit/capitulo/PresencaUsersTagLibTests.groovy
@@ -0,0 +1,17 @@
+package capitulo
+
+import grails.test.*
+
+class PresencaUsersTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
